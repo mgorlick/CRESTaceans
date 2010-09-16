bcf5a4f2d27d12cf4fecb7399b8d2e59ce566279
Kyle Strasser <kstrasse@ics.uci.edu>
2010-09-16 14:17:32 -0700

    test3 uses gstreamer DSL

--- bindings/gstreamer/tests/test3.rkt
+++ bindings/gstreamer/tests/test3.rkt
@@ -2,17 +2,17 @@
 #lang racket
 
 (require ffi/unsafe
-         "../gst/gstreamer.rkt")
+         "../gstreamer.rkt")
 (provide (all-defined-out))
 
 ; port of the ghost pad example from gstreamer docs
 
-(gst_init #f #f)
-
-(let ([sink (gst_element_factory_make "fakesink" "sink")]
-      [bin (cast (gst_bin_new "mybin") _GstElement-pointer _GstBin-pointer)])
-  (gst_bin_add bin sink)
-  (let ([pad (gst_element_get_static_pad sink "sink")])
-    (gst_element_add_pad bin (gst_ghost_pad_new "sink" pad))
-    (gst_object_unref pad))
-  (gst_object_unref bin))
+(with-gst-init
+ #f
+ (let ([sink (gst_element_factory_make "fakesink" "sink")]
+       [bin (cast (gst_bin_new "mybin") _GstElement-pointer _GstBin-pointer)])
+   (gst_bin_add bin sink)
+   (let ([pad (gst_element_get_static_pad sink "sink")])
+     (gst_element_add_pad bin (gst_ghost_pad_new "sink" pad))
+     (gst_object_unref pad))
+   (gst_object_unref bin)))
