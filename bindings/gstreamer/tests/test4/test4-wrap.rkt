ebca1c2111af43a25b3a35d02c4f38a64fd46d63
Kyle Strasser <=kstrasse@ics.uci.edu>
2011-02-04 15:38:50 -0800

    Made changes to test4 FFI wrapper to be consistent with changes in a previous commit

--- bindings/gstreamer/tests/test4/test4-wrap.rkt
+++ bindings/gstreamer/tests/test4/test4-wrap.rkt
@@ -5,7 +5,7 @@
          "../../gst/gstreamer.rkt")
 (provide (except-out (all-defined-out) test4-wrap df-t4))
 
-(define test4-wrap (ffi-lib "libracketgst-test4" "1.0"))
+(define test4-wrap (ffi-lib "libracket-gst-test4" "1.0"))
 (define-ffi-definer df-t4 test4-wrap)
 
 (df-t4 t4_signal_connect (_fun _GstElement-pointer ((_or-null _GstElement-pointer) = #f) -> _gulong))
