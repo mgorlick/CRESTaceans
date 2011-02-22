1c39a1a0dbab9ccbe5291d6564f9528bd55aaf34
Kyle Strasser <kstrasse@ics.uci.edu>
2011-02-22 10:00:26 -0800

    minimal amount of bindings to build decoder

---
+++ bindings/vorbis/libvorbis.rkt
@@ -0,0 +1,126 @@
+#lang racket
+
+(require ffi/unsafe
+         "oggpack.rkt")
+(provide (all-defined-out)
+         (all-from-out "oggpack.rkt"))
+
+(define libvorbis (ffi-lib "libvorbis"))
+
+(define-syntax-rule (defvorbis obj typ)
+  (define obj (get-ffi-obj (regexp-replaces 'obj '((#rx"-" "_"))) libvorbis typ)))
+(define-syntax-rule (defvorbis* typ obj ...)
+  (begin (defvorbis obj typ)
+         ...))
+
+(define-cstruct _alloc-chain
+  ([ptr _pointer] ; XXX problematic
+   [next _alloc-chain-pointer]))
+
+(define-cstruct _vorbis-info
+  ([version _int]
+   [channels _int]
+   [rate _long]
+   [bitrate-upper _long]
+   [bitrate-nominal _long]
+   [bitrate-lower _long]
+   [bitrate-window _long]
+   [codec-setup _pointer])) ; XXX problematic
+
+(define-cstruct _vorbis-dsp-state
+  ([analysisp _int]
+   [vi _vorbis-info-pointer]
+   [pcm (_ptr io (_ptr io _float))]
+   [pcmret (_ptr io (_ptr io _float))]
+   [pcm-storage _int]
+   [pcm-current _int]
+   [pcm-returned _int]
+   [preextrapolate _int]
+   [eofflag _int]
+   [lW _long]
+   [W _long]
+   [nW _long]
+   [centerW _long]
+   [granulepos _ogg_int64]
+   [sequence _ogg_int64]
+   [glue-bits _ogg_int64]
+   [time-bits _ogg_int64]
+   [floor-bits _ogg_int64]
+   [res-bits _ogg_int64]
+   [backend-state _pointer])) ; XXX problematic
+
+(define-cstruct _vorbis-block
+  ([pcm (_ptr io (_ptr io _float))]
+   [opb _oggpack-buffer]
+   [lW _long]
+   [W _long]
+   [nW _long]
+   [pcmend _int]
+   [mode _int]
+   [eofflag _int]
+   [granulepos _ogg_int64]
+   [sequence _ogg_int64]
+   [vd _vorbis-dsp-state-pointer]
+   [localstore _pointer] ; XXX problematic
+   [localtop _long]
+   [localalloc _long]
+   [totaluse _long]
+   [reap _alloc-chain-pointer]
+   [glue-bits _long]
+   [time-bits _long]
+   [floor-bits _long]
+   [res-bits _long]
+   [internal _pointer])) ; XXX problematic
+
+(define-cstruct _vorbis-comment
+  ([user-comments (_ptr io _string)]
+   [comment-lengths (_ptr io _int)]
+   [comments _int]
+   [vendor _string]))
+
+(define OV_FALSE -1)
+(define OV_EOF -2)
+(define OV_HOLE -3)
+(define OV_EREAD -128)
+(define OV_EFAULT -129)
+(define OV_EIMPL -130)
+(define OV_EINVAL -131)
+(define OV_ENOTVORBIS -132)
+(define OV_EBADHEADER -133)
+(define OV_EVERSION -134)
+(define OV_ENOTAUDIO -135)
+(define OV_EBADPACKET -136)
+(define OV_EBADLINK -137)
+(define OV_ENOSEEK -138)
+
+;; codec.h
+;; general
+(defvorbis* (_fun _vorbis-info-pointer -> _void)
+  vorbis-info-init
+  vorbis-info-clear)
+(defvorbis vorbis-block-init (_fun _vorbis-dsp-state-pointer _vorbis-block-pointer -> _int))
+(defvorbis vorbis-block-clear (_fun _vorbis-block-pointer -> _int))
+(defvorbis* (_fun _vorbis-comment-pointer -> _void)
+  vorbis-comment-init
+  vorbis-comment-clear)
+
+;; synthesis layer
+(defvorbis vorbis-synthesis-idheader (_fun _ogg-packet-pointer -> _int))
+(defvorbis vorbis-synthesis-headerin
+  (_fun _vorbis-info-pointer _vorbis-comment-pointer _ogg-packet-pointer -> _int))
+(defvorbis vorbis-synthesis-init
+  (_fun _vorbis-dsp-state-pointer _vorbis-info-pointer -> _int))
+(defvorbis vorbis-synthesis-restart (_fun _vorbis-dsp-state-pointer -> _int))
+(defvorbis vorbis-synthesis-blockin
+  (_fun _vorbis-dsp-state-pointer _vorbis-block-pointer -> _int))
+(defvorbis vorbis-synthesis-read (_fun _vorbis-dsp-state-pointer _int -> _int))
+(defvorbis vorbis-packet-blocksize (_fun _vorbis-info-pointer _ogg-packet-pointer -> _long))
+(defvorbis vorbis-synthesis-halfrate (_fun _vorbis-info-pointer _int -> _int))
+(defvorbis vorbis-synthesis-halfrate-p (_fun _vorbis-info-pointer -> _int))
+
+(defvorbis* (_fun _vorbis-block-pointer _ogg-packet-pointer -> _int)
+  vorbis-synthesis
+  vorbis-synthesis-trackonly)
+(defvorbis* (_fun _vorbis-dsp-state-pointer (_ptr io (_ptr io _float)) -> _int)
+  vorbis-synthesis-pcmout
+  vorbis-synthesis-lapout)
---
+++ bindings/vorbis/oggpack.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require ffi/unsafe)
+(provide (all-defined-out))
+
+(define _ogg_int64 _int64)
+
+(define-cstruct _oggpack-buffer
+  ([endbyte _long]
+   [endbit _long]
+   [buffer _string]
+   [ptr _string]
+   [storage _long]))
+
+(define-cstruct _ogg-packet
+  ([packet _bytes]
+   [bytes _long]
+   [b-o-s _long]
+   [e-o-s _long]
+   [granulepos _ogg_int64]
+   [packetno _ogg_int64]))
