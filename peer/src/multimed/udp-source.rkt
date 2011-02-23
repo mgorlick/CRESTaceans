9f2c087ae199e05221124ab6731319201fcbbd41
Kyle Strasser <kstrasse@ics.uci.edu>
2011-02-23 12:51:12 -0800

    Marching steadily along...

---
+++ peer/src/multimed/udp-source.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(require "util.rkt"
+         (planet bzlib/thread:1:0))
+
+(provide udp-source)
+
+;; UDP source component
+
+(define (udp-socket port)
+  (let ([s (udp-open-socket)])
+    (udp-bind! s #f port)
+    s))
+
+(define/contract (udp-source port . sinks)
+  ([number?] #:rest (listof thread?) . ->* . void)
+  (let ([sock (udp-socket port)]
+        [buffer (make-bytes 10000)])
+    (let loop ()
+      (let*-values 
+          ([(len addr port) (udp-receive! sock buffer)]
+           [(subbuffer) (bytes->immutable-bytes (subbytes buffer 0 len))])
+        (to-all sinks <- subbuffer)
+        (loop)))))
