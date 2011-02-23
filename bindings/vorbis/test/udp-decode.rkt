a7a1d0933226dfa6b78c423a71eb11f8b556a519
Kyle Strasser <kstrasse@ics.uci.edu>
2011-02-22 16:11:34 -0800

    ease of configuration

--- bindings/vorbis/test/udp-decode.rkt
+++ bindings/vorbis/test/udp-decode.rkt
@@ -19,18 +19,23 @@
 (define (go)
   (let* ([pid (current-thread)]
          [t (thread (λ () (udp-source 44000 (list pid))))])
-    (let loop ()
+    (let loop ([c 8])
       (receive/match
        [(list (? thread? thd)
               (? (and bytes? immutable?) buffer)
               (? number? len))
         (bytes->ogg-packet buffer len)
-        (loop)]))))
+        (when (> c 0) (loop (sub1 c)))
+        ]))))
+
+(define signed? #t)
+(define big? #f)
 
 (define (bytes->ogg-packet buffer len)
   (cond
     [(< len 28) (printf "weird packet: ~a bytes~n" len)]
     [else
+     (printf "packet len = ~a~n" len)
      (let* ([data (subbytes buffer 0 (- len (+ (* 2 ogg64size) (* 3 longsize))))]
             [len+ (curry + (bytes-length data))]
             [bytes (subbytes buffer (len+ 0) (len+ longsize))]
@@ -38,8 +43,7 @@
             [eos (subbytes buffer (len+ (* 2 longsize)) (len+ (* 3 longsize)))]
             [granulepos (subbytes buffer (len+ (* 3 longsize)) (len+ (* 3 longsize) ogg64size))]
             [packetno (subbytes buffer (len+ (* 3 longsize) ogg64size) len)])
-       (printf "data: ~a~n" data)
-       (printf "packet len: ~a~n" (integer-bytes->integer bytes #t)))
-     ]))
+       
+       'x)]))
 
 (go)
--- bindings/vorbis/test/udp-fake-tester.rkt
+++ bindings/vorbis/test/udp-fake-tester.rkt
@@ -2,13 +2,16 @@
 
 (require "../libvorbis/libvorbis.rkt")
 
+(define signed? #t)
+(define big? #f)
+
 (define (ogg-packet->bytes o)
   (bytes-append (ogg-packet-packet o)
-                (integer->integer-bytes (ogg-packet-bytes o) longsize #t)
-                (integer->integer-bytes (ogg-packet-b-o-s o) longsize #t)
-                (integer->integer-bytes (ogg-packet-e-o-s o) longsize #t)
-                (integer->integer-bytes (ogg-packet-granulepos o) ogg64size #t)
-                (integer->integer-bytes (ogg-packet-packetno o) ogg64size #t)))
+                (integer->integer-bytes (ogg-packet-bytes o) longsize signed? big?)
+                (integer->integer-bytes (ogg-packet-b-o-s o) longsize signed? big?)
+                (integer->integer-bytes (ogg-packet-e-o-s o) longsize signed? big?)
+                (integer->integer-bytes (ogg-packet-granulepos o) ogg64size signed? big?)
+                (integer->integer-bytes (ogg-packet-packetno o) ogg64size signed? big?)))
 
 (define my-packet (make-ogg-packet #"ABCDEFGHI" 37 0 0 500 1))
 (define my-packet-bytes (ogg-packet->bytes my-packet))
