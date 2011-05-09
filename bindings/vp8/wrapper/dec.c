767b829cc3a56be778067fe01738479665a1b7e1
Kyle Strasser <kstrasse@ics.uci.edu>
2011-05-09 14:26:55 -0700

    Incremental fixes on I420 conversion and display

--- bindings/vp8/wrapper/dec.c
+++ bindings/vp8/wrapper/dec.c
@@ -15,6 +15,9 @@ typedef struct VP8Dec {
   SDL_Rect rect;
 } VP8Dec;
 
+int init_video (VP8Dec *dec);
+void display_video (VP8Dec *dec, vpx_image_t *img);
+
 /* XXX fixme vp8dec_delete */
 
 VP8Dec* vp8dec_new (void) {  
@@ -35,35 +38,120 @@ VP8Dec* vp8dec_new (void) {
   return dec;
 }
 
+/* useful for debugging */
+void peek (size_t size, unsigned char* data) {
+  
+  vpx_codec_stream_info_t stream_info;
+  vpx_codec_err_t status;
+  
+  memset (&stream_info, 0, sizeof (stream_info));
+  stream_info.sz = sizeof (stream_info);
+  
+  status = vpx_codec_peek_stream_info (&vpx_codec_vp8_dx_algo, data, size, &stream_info);
+  if (!stream_info.is_kf) {
+    printf ("Not a keyframe. Skipping...\n");
+    return;
+  }
+  printf ("stream info? %d %d %d %d\n", stream_info.w, stream_info.h,
+          stream_info.sz, stream_info.is_kf);
+  if (status != VPX_CODEC_OK) {
+    printf ("Error getting stream info: %s\n",  vpx_codec_err_to_string (status));
+  }
+}
+
+int vp8dec_init (VP8Dec *dec, size_t size,
+                 unsigned char *data) {
+
+  vpx_codec_stream_info_t si;
+  vpx_codec_err_t status;
+  int flags = 0;
+
+  memset (&si, 0, sizeof (si));
+  si.sz = sizeof (si);
+  status = vpx_codec_peek_stream_info (&vpx_codec_vp8_dx_algo, data, size, &si);
+  
+  if (!si.is_kf) goto not_kf;
+  if (status != VPX_CODEC_OK) goto no_stream_info;
+
+  printf ("Decoder: stream info? %d %d %d %d\n", si.w, si.h, si.sz, si.is_kf);
+  dec->width = si.w;
+  dec->height = si.h;
+
+  status = vpx_codec_dec_init (dec->codec, &vpx_codec_vp8_dx_algo, NULL, flags);
+  if (status != VPX_CODEC_OK) goto no_init;
+
+  dec->is_init = 1;
+  return 1;
+
+not_kf:
+  /* arises when we've tuned into the middle of a stream. no problem,
+     just wait for a keyframe to arrive. */
+  return 0;
+
+no_stream_info:
+  /* stream info is junk...eventually should give the user a chance
+     to decide whether to continue, but for now just loop */
+  printf ("Decoder: Error getting stream info: %s\n", vpx_codec_err_to_string (status));
+  return 0;
+
+no_init:
+  /* probably a critical error with VPX itself. don't expect this to happen. */
+  printf ("Failed to initialize vp8 decoder: %s\n", vpx_codec_err_to_string (status));
+  return 0;
+
+}
+
+int vp8dec_decode (VP8Dec *dec, size_t size,
+                   unsigned char *data) {
+  vpx_image_t *img;
+  vpx_codec_err_t status;
+  vpx_codec_iter_t iter = NULL;
+  
+  if (0 == dec->is_init) {
+    vp8dec_init (dec, size, data);
+    init_video (dec);
+  }
+
+  if (0 == dec->is_init || dec->screen == NULL) goto not_initialized;
+
+  status = vpx_codec_decode (dec->codec, data, size, NULL, 0);
+
+  if (status != VPX_CODEC_OK) goto no_decode;
+
+  while (NULL != (img = vpx_codec_get_frame (dec->codec, &iter)))
+    display_video (dec, img);
+
+  return 1;
+
+not_initialized:
+  printf ("Skipping frame. Decoder not initialized yet.\n");
+  return 0;
+
+no_decode:
+  printf ("Failed to decode frame: %s\n", vpx_codec_err_to_string (status));
+  return 0;
+
+}
 int init_video (VP8Dec *dec) {
 
   int w, h;
-
   w = (dec->width + 1 & ~1) - (0 & ~1);
   h = (dec->height + 1 & ~1) - (0 & ~1);
 
-  printf ("setting video mode\n");
-
   dec->screen = SDL_SetVideoMode (w, h, 0, SDL_SWSURFACE);
-
   if (dec->screen == NULL) goto screen_init_err;
-
-  printf ("setting up overlay\n");
-
   dec->yuv_overlay = SDL_CreateYUVOverlay (w, h, SDL_YV12_OVERLAY, dec->screen);
-
   if (dec->yuv_overlay == NULL) goto overlay_init_err;
 
   dec->rect.x = 0;
   dec->rect.y = 0;
   dec->rect.w = w;
   dec->rect.h = h;
-
-  /*SDL_DisplayYUVOverlay (dec->yuv_overlay, &(dec->rect));*/
   return 1;
   
 screen_init_err:
   printf ("Error initializing SDL screen: %s\n", SDL_GetError());
+  return 0;
   
 overlay_init_err:
   printf ("Error initializing SDL overlay: %s\n", SDL_GetError());
@@ -87,7 +175,7 @@ void display_video (VP8Dec *dec, vpx_image_t *img) {
   /* problems may exist for odd frame rect for some encodings */
 
   y_offset = (0 & ~1) + img->stride[0] * (0 & ~1);
-  uv_offset = (0/2) + (img->stride[1]) * (0/2);
+  uv_offset = img->stride[1];
   
   for (i = 0; i < dec->yuv_overlay->h; i++)
     memcpy (dec->yuv_overlay->pixels[0] + dec->yuv_overlay->pitches[0] * i,
@@ -105,81 +193,5 @@ void display_video (VP8Dec *dec, vpx_image_t *img) {
   if (SDL_MUSTLOCK(dec->screen)) SDL_UnlockSurface(dec->screen);
   SDL_UnlockYUVOverlay(dec->yuv_overlay);
   SDL_DisplayYUVOverlay(dec->yuv_overlay, &(dec->rect));
-}
-
-int vp8dec_init (VP8Dec *dec, size_t size,
-                 unsigned char *data) {
-
-  vpx_codec_stream_info_t stream_info;
-  vpx_codec_err_t status;
-  int flags = 0;
-
-  memset (&stream_info, 0, sizeof (stream_info));
-  stream_info.sz = sizeof (stream_info);
-  
-  status = vpx_codec_peek_stream_info (&vpx_codec_vp8_dx_algo, data, size, &stream_info);
-  
-  if (!stream_info.is_kf) {
-    printf ("Decoder: Not a keyframe. Skipping...\n");
-    return 0;
-  }
-
-  printf ("Decoder: stream info? %d %d %d %d\n", stream_info.w,
-          stream_info.h,
-          stream_info.sz,
-          stream_info.is_kf);
-      
-
-  if (status != VPX_CODEC_OK) {
-    printf ("Decoder: Error getting stream info: %s\n", vpx_codec_err_to_string (status));
-    return 0;
-  }
-
-  dec->width = stream_info.w;
-  dec->height = stream_info.h;
-  /* XXX fixme address pixel format */
-
-  status = vpx_codec_dec_init (dec->codec, &vpx_codec_vp8_dx_algo, NULL, flags);
-  
-  if (status != VPX_CODEC_OK) {
-    printf ("Failed to initialize vp8 decoder: %s\n", vpx_codec_err_to_string (status));
-    return 0;
-  }
-
-  init_video (dec);
-  dec->is_init = 1;
-  return 1;
-}
-
-int vp8dec_decode (VP8Dec *dec, size_t size,
-                   unsigned char *data) {
-  vpx_image_t *img;
-  vpx_codec_err_t status;
-  vpx_codec_iter_t iter = NULL;
-  
-  if (0 == dec->is_init) vp8dec_init (dec, size, data);
-
-  if (0 == dec->is_init)
-    goto not_initialized;
-
-  status = vpx_codec_decode (dec->codec, data, size, NULL, 0);
-
-  if (status != VPX_CODEC_OK) {
-    printf ("Failed to decode frame: %s\n", vpx_codec_err_to_string (status));
-    return 0;
-  }
-
-  while (NULL != (img = vpx_codec_get_frame (dec->codec, &iter))) {
-    /* XXX fixme display it */
-    printf ("Some image info: \t\nPixel format = %d\n\twxh: %dx%d\n",
-            img->fmt, img->w, img->h);
-    display_video (dec, img);
-  }
-
-  return 1;
-
-not_initialized:
-  /* printf ("Skipping frame. Decoder not initialized yet.\n"); */
-  return 0;
 }
 
--- bindings/vp8/wrapper/enc.c
+++ bindings/vp8/wrapper/enc.c
@@ -4,8 +4,6 @@
 
 #include <vpx/vpx_encoder.h>
 #include <vpx/vp8cx.h>
-#include <vpx/vp8dx.h>
-#include <vpx/vpx_decoder.h>
 
 #include "enc_settings.h"
 
@@ -17,6 +15,7 @@
 #define DIV_ROUND_UP_X(v,x) (((v) + GEN_MASK(x)) >> (x))
 
 inline int get_offset (int component_index, int pic_width, int pic_height);
+inline int get_row_stride (int component_index, int pic_width);
 static void yuv422_to_yuv420p (unsigned char *dest, unsigned char *src,
                                int width, int height);
 
@@ -27,8 +26,6 @@ typedef struct VP8Enc {
   int height;
   int n_frames;
 } VP8Enc;
-
-typedef void (*vp8enc_foreach_frame) (size_t size, unsigned char *buffer);
 
 VP8Enc* vp8enc_new (void) {
   int i;
@@ -51,13 +48,17 @@ VP8Enc* vp8enc_new (void) {
 
   enc->width = cfg.g_w;
   enc->height = cfg.g_h;
-  enc->n_frames = 1;
+  enc->n_frames = 0;
 
   vpx_img_alloc (&enc->image, VPX_IMG_FMT_I420, cfg.g_w, cfg.g_h, 0);
+  enc->image.planes[0] = enc->image.img_data;
+  for (i = 1; i < 3; i++) {
+    printf ("planes[%d] offset = %d\n", i, get_offset (i, cfg.g_w, cfg.g_h));
+    printf ("planes[%d] stride = %d\n", i, get_row_stride (i, cfg.g_w));
+    enc->image.planes[i] = enc->image.planes[0] + get_offset (i, cfg.g_w, cfg.g_h);
+    enc->image.stride[i] = get_row_stride (i, cfg.g_w);
+  }
   
-  for (i = 0; i < 3; i++)
-    enc->image.planes[i] = enc->image.planes[0] +
-        get_offset (i, enc->image.w, enc->image.h);
   status = vpx_codec_enc_init (&enc->codec, &vpx_codec_vp8_cx_algo, &cfg, 0);
   if (status != VPX_CODEC_OK) goto no_init;
 
@@ -73,7 +74,7 @@ no_enc_alloc:
   
 no_init:
   printf ("Couldn't initialize codec: %s\n", vpx_codec_err_to_string (status));
-  vpx_img_free (&(enc->image));
+  vpx_img_free (&enc->image);
   free (enc);
   return NULL;
 }
@@ -82,26 +83,15 @@ no_init:
 void buffer_to_image_YUYV (size_t size, unsigned char *buffer, vpx_image_t *image) {
 
   int delta_cb, delta_cr;
-
+  image->x_chroma_shift = image->y_chroma_shift = 1;
+  
   delta_cb = ROUND_UP_4 (image->w) * ROUND_UP_X (image->h, 1);
   delta_cr = ROUND_UP_4 (DIV_ROUND_UP_X (image->w, 1))
       * DIV_ROUND_UP_X (image->h, 1);
 
-  image->x_chroma_shift = image->y_chroma_shift = 1;
-
   image->planes[VPX_PLANE_Y] = buffer;
   image->planes[VPX_PLANE_U] = image->planes[VPX_PLANE_Y] + delta_cb;
   image->planes[VPX_PLANE_V] = image->planes[VPX_PLANE_U] + delta_cr;
-  
-  printf ("wxh: %dx%d / dwxdw: %dx%d\n", image->w, image->h, image->d_w, image->d_h);
-  printf ("xcs: %d, ycs: %d\n", image->x_chroma_shift, image->y_chroma_shift);
-  printf ("bps: %d\n", image->bps);
-  printf ("Cb - Y = %d, Cr - Cb = %d\n",
-          image->planes[VPX_PLANE_U] - image->planes[VPX_PLANE_Y],
-          image->planes[VPX_PLANE_V] - image->planes[VPX_PLANE_U]);
-  printf ("strides: %d, %d, %d\n",
-          image->stride[VPX_PLANE_Y], image->stride[VPX_PLANE_U],
-          image->stride[VPX_PLANE_V]);
 }
 
 inline int get_offset (int component_index, int pic_width, int pic_height) {
@@ -119,32 +109,10 @@ inline int get_offset (int component_index, int pic_width, int pic_height) {
   }
 }
 
-void peek (size_t size, unsigned char* data) {
-  
-  vpx_codec_stream_info_t stream_info;
-  vpx_codec_err_t status;
-  
-  memset (&stream_info, 0, sizeof (stream_info));
-  stream_info.sz = sizeof (stream_info);
-  
-  status = vpx_codec_peek_stream_info (&vpx_codec_vp8_dx_algo,
-                                       data, size, &stream_info);
-  
-  if (!stream_info.is_kf) {
-    printf ("Encoder: Not a keyframe. Skipping...\n");
-    return;
-  }
-
-    printf ("Encoder: stream info? %d %d %d %d\n", stream_info.w,
-          stream_info.h,
-          stream_info.sz,
-          stream_info.is_kf);
-      
-
-  if (status != VPX_CODEC_OK) {
-    printf ("Encoder: Error getting stream info: %s\n",
-            vpx_codec_err_to_string (status));
-  }
+inline int get_row_stride (int component_index, int pic_width) {
+  return (component_index == 0) ?
+      ROUND_UP_4 (pic_width) :
+      ROUND_UP_4 (ROUND_UP_2 (pic_width) / 2);
 }
 
 int vp8enc_encode (VP8Enc *enc, size_t size, unsigned char *buffer,
@@ -155,16 +123,15 @@ int vp8enc_encode (VP8Enc *enc, size_t size, unsigned char *buffer,
   vpx_codec_iter_t iter = NULL;
   int flags = 0;
   
-  yuv422_to_yuv420p (enc->image.img_data, buffer, enc->image.w, enc->image.h);
+  yuv422_to_yuv420p (enc->image.img_data, buffer, enc->width, enc->height);
   
   /* XXX fixme set force-keyframe flag only when commanded */
   flags |= VPX_EFLAG_FORCE_KF;
   
-  status = vpx_codec_encode (&enc->codec, &enc->image, enc->n_frames++, 1, flags, 0);
+  status = vpx_codec_encode (&enc->codec, &enc->image, enc->n_frames++, 1, flags, 10000);
   if (status != VPX_CODEC_OK) goto no_frame;
   
   while (NULL != (pkt = vpx_codec_get_cx_data (&enc->codec, &iter))) {
-    
     switch (pkt->kind) {
       case VPX_CODEC_CX_FRAME_PKT:
         memcpy (out, pkt->data.frame.buf, pkt->data.frame.sz);
@@ -181,12 +148,6 @@ int vp8enc_encode (VP8Enc *enc, size_t size, unsigned char *buffer,
 no_frame:
   printf ("Couldn't encode buffer: %s\n", vpx_codec_err_to_string (status));
   return 0;
-}
-
-inline int get_row_stride (int component_index, int pic_width) {
-  return (component_index == 0) ?
-      ROUND_UP_4 (pic_width) :
-      ROUND_UP_4 (ROUND_UP_2 (pic_width) / 2);
 }
 
 /* this function pieced together from gstreamer ffmpegcolorspace component */
