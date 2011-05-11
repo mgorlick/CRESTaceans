033c2025cc023806c721776a54ca399bd7e4fa4f
Kyle Strasser <kstrasse@ics.uci.edu>
2011-05-11 15:00:11 -0700

    Multiple fork add/remove

--- peer/src/multimed/vp8/enc.rkt
+++ peer/src/multimed/vp8/enc.rkt
@@ -5,7 +5,6 @@
          "../udtsink.rkt"
          "../fork.rkt"
          "vp8enc.rkt"
-         "vp8dec.rkt"
          "v4l2-reader.rkt")
 
 (provide (all-defined-out)
@@ -13,11 +12,11 @@
 
 (define pid (current-thread))
 
-(define p (make-pipeline (["vp8dec"      : t4 (make-vp8-decoder pid)]
-                          ["fork"        : t3 (make-fork pid (list t4))]
-                          ["vp8"         : t2 (make-vp8-encoder pid t3)]
-                          ["v4l2-reader" : t1 (make-v4l2-reader pid t2)])))
+(define p (make-pipeline (["fork" : t3 (make-fork pid (list))]
+                          ["vp8"  : t2 (make-vp8-encoder pid t3)]
+                          ["v4l2" : t1 (make-v4l2-reader pid t2)])))
 
+(define t4 (thread (make-udt-writer pid "128.195.58.146" 5000)))
 (define t5 (thread (make-udt-writer pid "127.0.0.1" 5000)))
 
 (define fork (dict-ref p "fork"))
