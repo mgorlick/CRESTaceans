7c1584101d0ef42dca7a0f12a13bfa8bafe80cfb
Kyle Strasser <kstrasse@ics.uci.edu>
2011-07-06 06:34:11 -0700

    Add audio. Still requires synchronization (webM?)

--- apps/multimedia/decoder.rkt
+++ apps/multimedia/decoder.rkt
@@ -2,13 +2,15 @@
 #lang racket/base
 
 (require "pipeline/vp8dec.rkt"
+         "pipeline/vorbisdec.rkt"
          "../../peer/src/net/tcp-peer.rkt"
          "../../peer/src/net/structs.rkt"
          "../../peer/src/api/compilation.rkt"
          "../../peer/src/api/message.rkt"
          racket/match)
 
-(define scurl->public-scurl-string (compose scurl->string scurl->public-scurl))
+(define (synthesize-scurl-string path)
+  (scurl->string (scurl->public-scurl (generate-scurl/defaults *LOCALHOST* *LOCALPORT* #:key k #:path path))))
 
 (define *RHOST* *LOCALHOST*)
 (define *RPORT* 5000)
@@ -24,15 +26,22 @@
 (define request-thread (run-tcp-peer *LOCALHOST* *LOCALPORT* this-scurl me))
 
 (printf "Listening on ~a~n" (regexp-split "/" (scurl->string this-scurl)))
-(define decoder0 (thread (make-vp8-decoder me)))
-(define decoder0-curl (scurl->public-scurl-string (generate-scurl/defaults *LOCALHOST* *LOCALPORT* #:key k #:path "decoder0")))
-(hash-set! curls=>threads decoder0-curl decoder0)
-(printf "video0 decoder installed at ~a~n" decoder0-curl)
+
+(define video0-curl (synthesize-scurl-string "video0"))
+(hash-set! curls=>threads video0-curl (thread (make-vp8-decoder me)))
+(define audio0-curl (synthesize-scurl-string "audio0"))
+(hash-set! curls=>threads audio0-curl (thread (make-vorbis-decoder me)))
+(printf "video0 decoder installed at ~a~n" video0-curl)
+(printf "audio0 decoder installed at ~a~n" audio0-curl)
 
 (let loop ()
   (match (thread-receive)
-    [(vector '<tuple> '(mischief message ask) #"POST" "/video0" body metadata reply-curl echo)
+    [(vector '<tuple> '(mischief message ask) "POST" "/video0" body metadata reply-curl echo)
      (if (bytes? body)
-         (thread-send (hash-ref curls=>threads decoder0-curl) body)
+         (thread-send (hash-ref curls=>threads video0-curl) body)
+         (printf "body is not bytes: ~a~n" body))]
+    [(vector '<tuple> '(mischief message ask) "POST" "/audio0" body metadata reply-curl echo)
+     (if (bytes? body)
+         (thread-send (hash-ref curls=>threads audio0-curl) body)
          (printf "body is not bytes: ~a~n" body))])
   (loop))
--- apps/multimedia/encoder.rkt
+++ apps/multimedia/encoder.rkt
@@ -3,6 +3,8 @@
 
 (require "pipeline/v4l2-reader.rkt"
          "pipeline/vp8enc.rkt"
+         "pipeline/vorbisenc.rkt"
+         "pipeline/pulsesrc.rkt"
          "pipeline/structs.rkt"
          "../../peer/src/net/tcp-peer.rkt"
          "../../peer/src/net/structs.rkt"
@@ -36,13 +38,23 @@
 (define *RHOST* *LOCALHOST*)
 (define *RPORT* 1235)
 
-(define encoder0 (thread (make-vp8-encoder me me)))
-(define video0 (thread (make-v4l2-reader me encoder0)))
+(define (relayer targeturl)
+  (let loop ()
+    (match (thread-receive)
+      [(FrameBuffer buffer len λdisp)
+       (define frame (subbytes buffer 0 len))
+       (ask/send "POST" request-thread *RHOST* *RPORT* *RKEY* (subbytes buffer 0 len) #:compile? #f #:url targeturl)
+       (λdisp)
+       (loop)]
+      [(? bytes? buffer)
+       (ask/send "POST" request-thread *RHOST* *RPORT* *RKEY* buffer #:compile? #f #:url targeturl)
+       (loop)])))
 
-(let loop ()
-  (match (thread-receive)
-    [(FrameBuffer buffer len λdisp)
-     (define frame (subbytes buffer 0 len))
-     (λdisp)
-     (ask/send #"POST" request-thread *RHOST* *RPORT* *RKEY* (subbytes buffer 0 len) #:compile? #f #:url "/video0")
-     (loop)]))
+(define videorelay0 (thread (λ () (relayer "/video0"))))
+(define vp80 (thread (make-vp8-encoder me videorelay0)))
+(define video0 (thread (make-v4l2-reader me vp80)))
+(define audiorelay0 (thread (λ () (relayer "/audio0"))))
+(define vorbis0 (thread (make-vorbis-encoder me (encoder-settings 2 44100 1.0 'naive) audiorelay0)))
+(define pulse0 (thread (make-pulsesrc me vorbis0)))
+
+(no-return)
