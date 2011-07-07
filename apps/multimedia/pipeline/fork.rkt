2720f416617e51b296d00f0fac3b43652a3a0cad
Kyle Strasser <kstrasse@ics.uci.edu>
2011-07-06 18:33:59 -0700

    Rewrite buffer pool to avoid async channel latency. Remove type annotations from fork and delete typed modules since fork was the only typed module remaining.

--- apps/multimedia/pipeline/fork.rkt
+++ apps/multimedia/pipeline/fork.rkt
@@ -1,52 +1,44 @@
-#lang typed/racket/base
+#lang racket/base
 
-(require "util-types.rkt"
+(require "util.rkt"
          "structs.rkt"
-         (only-in typed/racket
-                  curry
-                  match-let))
-
-(require/typed "util.rkt"
-               [receive-killswitch/whatever ((Any -> Boolean) -> (U Symbol AddRcvr RmvRcvr Bytes FrameBuffer))])
+         racket/function
+         racket/match)
 
 (provide make-fork
          fork-add
          fork-remove)
 
-(define-type AddRcvr (Pair 'AddRcvr Thread))
-(define-type RmvRcvr (Pair 'RmvRcvr Thread))
-(define-predicate add-rcvr? AddRcvr)
-(define-predicate rmv-rcvr? RmvRcvr)
+(struct add-rcvr (thread))
+(struct rmv-rcvr (thread))
 
-(: fork-add (Thread Thread -> Void))
 (define (fork-add fork thd)
-  (thread-send fork (cons 'AddRcvr thd))
+  (thread-send fork (add-rcvr thd))
   (void))
 
-(: fork-remove (Thread Thread -> Void))
 (define (fork-remove fork thd)
-  (thread-send fork (cons 'RmvRcvr thd))
+  (thread-send fork (rmv-rcvr thread))
   (void))
 
-(: make-fork (Thread (Listof Thread) -> (-> Void)))
 (define (make-fork signaller thds)
   (define is-signaller? (make-thread-id-verifier signaller))
   (λ ()
     (let loop ([thds thds])
       (let ([m (receive-killswitch/whatever is-signaller?)])
-        (cond [(add-rcvr? m) (if (ormap (curry equal? (cdr m)) thds)
+        (cond [(add-rcvr? m) (if (ormap (curry equal? (add-rcvr-thread m)) thds)
                                  (loop thds)
-                                 (loop (cons (cdr m) thds)))]
-              [(rmv-rcvr? m) (loop (filter (compose not (curry equal? (cdr m))) thds))]
+                                 (loop (cons (add-rcvr-thread m) thds)))]
+              [(rmv-rcvr? m) (loop (filter (compose not (curry equal? (rmv-rcvr-thread m))) thds))]
               [(die? m) (for-each (curry command/killswitch signaller) thds)
                         (reply/state-report signaller #f)]
               
-              [(bytes? m) (for-each (curry (rev-params thread-send) m) thds)
+              [(bytes? m) (for-each (curryr thread-send m) thds)
                           (loop thds)]
+              
               ;; pooling buffers and copying an incoming buffer to N outgoing buffers doesn't scale
               ;; in this case it's better to just let the GC handle it
               [(FrameBuffer? m) (match-let ([(FrameBuffer inbytes size λdisposal) m])
-                                  (define frame (FrameBuffer (subbytes inbytes 0 size) size void))
-                                  (map (curry (rev-params thread-send) frame) thds)
+                                  (define frame (make-FrameBuffer (subbytes inbytes 0 size) size void))
+                                  (map (curryr thread-send frame) thds)
                                   (λdisposal)
                                   (loop thds))])))))
