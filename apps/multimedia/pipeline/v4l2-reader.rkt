9b16d148409b266f37da22e061745fc52ada2d89
Kyle Strasser <kstrasse@ics.uci.edu>
2011-07-11 08:37:12 -0700

    Video reader doesn't need to maintain separate pool representation.

--- apps/multimedia/pipeline/v4l2-reader.rkt
+++ apps/multimedia/pipeline/v4l2-reader.rkt
@@ -15,30 +15,21 @@
     (define v (v4l2-reader-setup))
     (define-values (w h fn fd buffer-ct) (v4l2-reader-get-params v))
     (thread-send receiver (make-VideoParams w h fn fd))
-    (define v-sema (make-semaphore 1)) ; protect v4l2 reader and pool representation
+    (define v-sema (make-semaphore 1)) ; protect v4l2 reader
     
-    (define in-pool-range/c (between/c 0 (sub1 buffer-ct)))
-    ; FIXME: turn contracts back on here once performance is tuned
-    (define pool (for/set ([i (in-range buffer-ct)]) i))
-    (define (pool-has? i) (set-member? pool i))
-    (define (pool-add! i) (set! pool (set-add pool i)))
-    (define (pool-remove! i) (set! pool (set-remove pool i)))
+    (define returners (for/vector ([i (in-range buffer-ct)]) (λ () (thread-send pool-helper i #f))))
     
     (define-thread pool-helper
       (let loop ()
         (let ([index (thread-receive)])
           (semaphore-wait v-sema)
-          (unless (pool-has? index)
-            (if (v4l2-reader-enqueue-buffer v index)
-                (pool-add! index)
-                (printf "error: couldn't requeue buffer #~a~n" index)))
+          (unless (v4l2-reader-enqueue-buffer v index)
+            (printf "error: couldn't requeue buffer #~a~n" index))
           (semaphore-post v-sema))
         (loop)))
     
     (define (make-frame data size i ts)
-      (define (requeue)
-        (thread-send pool-helper i #f))
-      (make-FrameBuffer data size requeue ts))
+      (make-FrameBuffer data size (vector-ref returners i) ts))
     
     (define is-signaller? (make-thread-id-verifier signaller))
     
@@ -50,15 +41,14 @@
         [(v4l2-reader-is-ready v) ; we have to do busy waiting for now - can't select() at a lower level -or- here
          (let-values ([(d f s i) (v4l2-reader-get-frame v)])
            (when d
-             (thread-send receiver (make-frame d s i ts)) ; throw away framenum for now
-             (pool-remove! i))
+             (thread-send receiver (make-frame d s i ts))) ; throw away framenum for now
            (semaphore-post v-sema))
          #t]
         [else
          (semaphore-post v-sema)
          (sleep sleep-time) ; sleep for a quarter of the frame duration
          #f]))
-        
+    
     (define (cleanup)
       (command/killswitch signaller receiver)
       (semaphore-wait v-sema)
@@ -66,7 +56,7 @@
       (semaphore-post v-sema)
       ;; HACK: we need to wait for the next elements
       ;; of the pipeline to free themselves before we kill
-      ;; the buffer so they're not writing to freed mmapped memory
+      ;; the buffer so they're not reading from freed mmapped memory
       ;; since this is video one second should be enough.
       (sleep 1)
       (v4l2-reader-delete v)
