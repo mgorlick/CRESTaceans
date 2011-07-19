ae45fabb5bb52258996cb5e933fbcc6427aaa68e
Kyle Strasser <kstrasse@ics.uci.edu>
2011-07-19 04:34:52 -0700

    Do (safe) pointer arithmetic on nacl bstrs instead of taking copies

--- bindings/nacl/libnacl/libnacl.rkt
+++ bindings/nacl/libnacl/libnacl.rkt
@@ -38,38 +38,61 @@
 (define zeroes (make-bytes crypto-box-ZEROBYTES))
 (define boxzeroes (make-bytes crypto-box-BOXZEROBYTES))
 
-(defnacl crypto-box-keypair-wrap (_fun (pk : (_bytes o crypto-box-PUBLICKEYBYTES))
-                                       (sk : (_bytes o crypto-box-SECRETKEYBYTES))
-                                       -> (r : _int) -> (values pk sk r)))
+(defnacl crypto-box-keypair-wrap
+  (_fun (pk : (_bytes o crypto-box-PUBLICKEYBYTES))
+        (sk : (_bytes o crypto-box-SECRETKEYBYTES))
+        -> (r : _int)
+        -> (values pk sk r)))
 
-(defnacl crypto-box-wrap (_fun (m n pk sk) ::
-                               (ciphertext : (_bytes o message-length))
-                               (message : _bytes = (bytes-append zeroes m))
-                               (message-length : _ullong = (bytes-length message))
-                               (n : _bytes) (pk : _bytes) (sk : _bytes)
-                               -> (r : _int) -> (values (subbytes ciphertext crypto-box-BOXZEROBYTES) r)))
+(defnacl crypto-box-wrap
+  (_fun (m n pk sk) ::
+        (ciphertext : (_bytes o message-length))
+        (message : _bytes = (bytes-append zeroes m))
+        (message-length : _ullong = (bytes-length message))
+        (n : _bytes) (pk : _bytes) (sk : _bytes)
+        -> (r : _int)
+        -> (values 
+            (cast (ptr-add ciphertext crypto-box-BOXZEROBYTES)
+                  _pointer (_bytes o (- message-length crypto-box-BOXZEROBYTES)))
+            r)))
 
-(defnacl crypto-box-open-wrap (_fun (c n pk sk) :: 
-                                    (message : (_bytes o cipher-length))
-                                    (ciphertext : _bytes = (bytes-append boxzeroes c))
-                                    (cipher-length : _ullong = (bytes-length ciphertext))
-                                    (n : _bytes) (pk : _bytes) (sk : _bytes)
-                                    -> (r : _int) -> (values (subbytes message crypto-box-ZEROBYTES) r)))
+(defnacl crypto-box-open-wrap
+  (_fun (c n pk sk) :: 
+        (message : (_bytes o cipher-length))
+        (ciphertext : _bytes = (bytes-append boxzeroes c))
+        (cipher-length : _ullong = (bytes-length ciphertext))
+        (n : _bytes) (pk : _bytes) (sk : _bytes)
+        -> (r : _int)
+        -> (values 
+            (cast (ptr-add message crypto-box-ZEROBYTES)
+                  _pointer (_bytes o (- cipher-length crypto-box-ZEROBYTES)))
+            r)))
 
-(defnacl crypto-box-beforenm-wrap (_fun (k : (_bytes o crypto-box-BEFORENMBYTES))
-                                        (pk : _bytes) (sk : _bytes)
-                                        -> (r : _int) -> (values k r)))
+(defnacl crypto-box-beforenm-wrap 
+  (_fun (k : (_bytes o crypto-box-BEFORENMBYTES))
+        (pk : _bytes) (sk : _bytes)
+        -> (r : _int) -> (values k r)))
 
-(defnacl crypto-box-afternm-wrap (_fun (m n k) ::
-                                       (ciphertext : (_bytes o message-length))
-                                       (message : _bytes = (bytes-append zeroes m)) 
-                                       (message-length : _ullong = (bytes-length message))
-                                       (n : _bytes) (k : _bytes)
-                                       -> (r : _int) -> (values (subbytes ciphertext crypto-box-BOXZEROBYTES) r)))
+(defnacl crypto-box-afternm-wrap 
+  (_fun (m n k) ::
+        (ciphertext : (_bytes o message-length))
+        (message : _bytes = (bytes-append zeroes m)) 
+        (message-length : _ullong = (bytes-length message))
+        (n : _bytes) (k : _bytes)
+        -> (r : _int)
+        -> (values 
+            (cast (ptr-add ciphertext crypto-box-BOXZEROBYTES)
+                  _pointer (_bytes o (- message-length crypto-box-BOXZEROBYTES)))
+            r)))
 
-(defnacl crypto-box-open-afternm-wrap (_fun (c n k) ::
-                                            (message : (_bytes o cipher-length))
-                                            (ciphertext : _bytes = (bytes-append boxzeroes c))
-                                            (cipher-length : _ullong = (bytes-length ciphertext))
-                                            (n : _bytes) (k : _bytes)
-                                            -> (r : _int) -> (values (subbytes message crypto-box-ZEROBYTES) r)))
+(defnacl crypto-box-open-afternm-wrap
+  (_fun (c n k) ::
+        (message : (_bytes o cipher-length))
+        (ciphertext : _bytes = (bytes-append boxzeroes c))
+        (cipher-length : _ullong = (bytes-length ciphertext))
+        (n : _bytes) (k : _bytes)
+        -> (r : _int)
+        -> (values 
+            (cast (ptr-add message crypto-box-ZEROBYTES)
+                  _pointer (_bytes o (- cipher-length crypto-box-ZEROBYTES)))
+            r)))
