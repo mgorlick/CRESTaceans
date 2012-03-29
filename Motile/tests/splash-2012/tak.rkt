e1f6217502e954afefb53a4a48a6fc95a4ed48cb
Kyle Strasser <kstrasse@ics.uci.edu>
2012-03-29 10:34:26 -0700

    Forgot to quote Motile source.

--- Motile/tests/splash-2012/tak.rkt
+++ Motile/tests/splash-2012/tak.rkt
@@ -14,14 +14,14 @@
 
 (define motile-compiled-source
   (motile/compile
-   (let ()
-     (define (tak x y z)
-       (if (not (< y x))
-           z
-           (tak (tak (- x 1) y z)
-                (tak (- y 1) z x)
-                (tak (- z 1) x y))))
-     (tak 18 12 6))))
+   '(let ()
+      (define (tak x y z)
+        (if (not (< y x))
+            z
+            (tak (tak (- x 1) y z)
+                 (tak (- y 1) z x)
+                 (tak (- z 1) x y))))
+      (tak 18 12 6))))
 
 (define (racket-version)
   (tak 18 12 6))
